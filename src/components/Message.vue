30c353f0957d36dd3409062a3c9d6e476e885eb8
kanaya-c <70326298+kanaya-c@users.noreply.github.com>
2020-11-04 19:08:42 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -4,24 +4,137 @@
       <div class="message">
         <div class="flex">
           <p class="name">{{value.name}}</p>
-          <img class="icon" src="../assets/heart.png" alt="">
+          <img class="icon" src="../assets/heart.png" @click="fav(key)" alt="">
           <p class="number">{{value.like.length}}</p>
-          <img class="icon" src="../assets/cross.png" alt="">
-          <img class="icon detail" src="../assets/detail.png" alt="">
+          <img 
+            class="icon" 
+            src="../assets/cross.png"
+            @click="del(key)"
+            alt=""
+            v-if="path && profile"
+            >
+          <img 
+            class="icon detail" 
+            src="../assets/detail.png"
+            @click="
+              $router.push({
+                path: '/detail/' + value.item.id,
+                params: { id: value.item.id},
+              })
+            "
+            alt=""
+            v-if="profile"
+            >
         </div>
-        <p class="text">{{value.share}}</p>
+        <p class="text">{{value.item.share}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  props: ["id"],
   data() {
     return {
-      shares: [{ name: "太郎", like: [], share: "初めまして" }]
+      // shares: [{ name: "太郎", like: [], share: "初めまして" }]
+      shares: [],
+      path: true,
+      profile: true,
     };
-  }
+  },
+  methods: {
+    fav(index) {
+      let result = this.shares[index].like.some((value) => {
+        return value.user_id == this.$store.state.user.id;
+      });
+      if (result) {
+        this.shares[index].like.forEach((element) => {
+          if (element.user_id == this.$store.state.user.id) {
+            axios({
+              method: "delete",
+              url: "https://glacial-meadow-49934.herokuapp.com/api/like",
+              data: {
+                share_id: this.shares[index].item.id,
+                user_id: this.$store.state.user.id,
+              },
+            }).then((response) => {
+              console.log(response);
+                this.$router.go({
+                  path: this.$router.currentRoute.path,
+                  force: true,
+                });
+            });
+          }
+        });
+      } else {
+        axios
+        .post("https://glacial-meadow-49934.herokuapp.com/api/like", {
+          share_id: this.shares[index].item.id,
+          user_id: this.$store.state.user.id,
+        })
+        .then((response) => {
+          console.log(response);
+          this.$router.go({
+            path: this.$router.currentRoute.path,
+            force: true,
+          });
+        });
+      }
+    },
+    del(index) {
+      axios
+      .delete(
+        "https://glacial-meadow-49934.herokuapp.com/api/shares/" +
+        this.shares[index].item.id
+      )
+      .then((response) => {
+        console.log(response);
+        this.$router.go({
+          path: this.$router.currentRoute.path,
+          force: true,
+        });
+      });
+    },
+    async getShares() {
+      let data = [];
+      let shares = await axios.get(
+        "https://glacial-meadow-49934.herokuapp.com/api/shares"
+      );
+      for (let i =0; i <shares.data.data.length; i++) {
+        await axios
+        .get(
+          "https://glacial-meadow-49934.herokuapp.com/api/shares/" +
+          shares.data.data[i].id
+        )
+        .then((response) => {
+          if(this.$router.name == "profile") {
+            if (response.data.item.user_id == this.$router.state.user.id) {
+              data.push(response.data);
+            }
+          } else if (this.$router.name == "detail") {
+            if (response.data.item.id == this.id) {
+              data.push(response.data);
+            }
+          } else {
+            data.push(response.data);
+          }
+        });
+      }
+      this.shares = data;
+      console.log(this.shares);
+    },
+  },
+  created() {
+    if (this.$router.name === "home") {
+      this.path = false;
+    }
+    if (this.$router.name === "detail") {
+      this.profile = false;
+    }
+    this.getShares();
+  },
 };
 </script>
 
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -14,10 +14,10 @@
         </div>
         <div class="message" v-for="(comment, index) in data" :key="index">
           <div class="flex">
-            <p class="name">{{comment.name}}</p>
+            <p class="name">{{comment.comment_user.name}}</p>
           </div>
           <div>
-            <p class="text">{{comment.content}}</p>
+            <p class="text">{{comment.comment.content}}</p>
           </div>
         </div>
         <input v-model="content" type="text">
@@ -32,18 +32,48 @@
 <script>
 import SlideNavi from "../components/SideNavi";
 import Message from "../components/Message";
+import axios from "axios";
 export default {
   props: ["id"],
   data() {
     return {
       content: "",
-      data: [{ name: "太郎", like: [], share: "初めまして" }]
+      data: "",
+      // data: [{ name: "太郎", like: [], share: "初めまして" }]
     };
+  },
+  methods: {
+    send() {
+      axios
+      .post("https://glacial-meadow-49934.herokuapp.com/api/comment", {
+        share_id: this.id,
+        user_id: this.$store.state.user.id,
+        content: this.content,
+      })
+      .them((response) => {
+        console.log(response);
+        this.content = "";
+        this.$router.go({
+          path: this.$router.currentRoute.path,
+          force: true,
+        });
+      });
+    },
+    comment() {
+      axios
+      .get("https://glacial-meadow-49934.herokuapp.com/api/shares/" +this.id)
+      .then((response) => {
+        this.data = response.data.comment;
+      });
+    },
+  },
+  created() {
+    this.comment();
   },
   components: {
     SlideNavi,
     Message
-  }
+  },
 };
 </script>
 
@@ -67,6 +97,9 @@ export default {
 .title p {
   font-size: 20px;
   font-weight: bold;
+}
+.share-message {
+  border-bottom: 1px solid white;
 }
 .commnet-title {
   text-align: center;
